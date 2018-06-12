f6b42a6ebc84ee537dc30bc943c370d241ca163d
pran <banerjee.pranay92@gmail.com>
2018-06-12 09:47:18 +0530

    updated code for setting splash screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform , ModalController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { SplashPage } from '../pages/splash/splash';
 
 import { HomePage } from '../pages/home/home';
 @Component({
@@ -10,12 +11,16 @@ import { HomePage } from '../pages/home/home';
 export class MyApp {
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, modalCtrl: ModalController) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
-      splashScreen.hide();
+      
+     // splashScreen.hide();
+     let splash = modalCtrl.create(SplashPage);
+     splash.present();
+
     });
   }
 }
